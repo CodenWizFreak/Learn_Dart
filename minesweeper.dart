fa6bccb83bf87c04cabb055a15da631fe98b1e6e
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-03-06 23:00:15 +0530

    Create minesweeper.dart

---
+++ minesweeper.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MinesweeperApp());
+}
+
+class MinesweeperApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Minesweeper',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MinesweeperGame(),
+    );
+  }
+}
