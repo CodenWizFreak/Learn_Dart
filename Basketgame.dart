3646dd1b68a7b679cf84b2e078228b1a25126477
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-03-08 00:08:02 +0530

    Update Basketgame.dart

--- Basketgame.dart
+++ Basketgame.dart
@@ -14,3 +14,19 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class FallingObjectGame extends StatefulWidget {
+  @override
+  _FallingObjectGameState createState() => _FallingObjectGameState();
+}
+
+class _FallingObjectGameState extends State<FallingObjectGame> {
+  double basketPosition = 0; // Horizontal position of the basket
+  double basketWidth = 100;
+  double basketHeight = 20;
+  double objectWidth = 50;
+  double objectHeight = 50;
+  double fallingSpeed = 5; // Speed at which the object falls
+  bool isGameOver = false;
+  int score = 0;
+  Timer? gameTimer;
+  List<PositionedObject> fallingObjects = []; // List to store the positions of falling objects
