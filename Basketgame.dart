7488ef6286b418aa6718267762dcc9aed7c000e8
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-03-08 00:08:19 +0530

    Update Basketgame.dart

--- Basketgame.dart
+++ Basketgame.dart
@@ -30,3 +30,29 @@ class _FallingObjectGameState extends State<FallingObjectGame> {
   int score = 0;
   Timer? gameTimer;
   List<PositionedObject> fallingObjects = []; // List to store the positions of falling objects
+// Start the game and update the falling objects and basket position
+  void startGame() {
+    gameTimer = Timer.periodic(Duration(milliseconds: 16), (timer) {
+      if (!isGameOver) {
+        setState(() {
+          // Update the position of each falling object
+          for (var obj in List.from(fallingObjects)) { // Create a copy to avoid modifying while iterating
+            obj.positionY += fallingSpeed;
+
+            // Check for collision with the basket
+            if (obj.positionY + objectHeight >= 600 - basketHeight &&
+                obj.positionX + objectWidth > basketPosition &&
+                obj.positionX < basketPosition + basketWidth) {
+              score++;
+              fallingObjects.remove(obj); // Remove object after it's caught
+            }
+
+            // Remove the object if it goes below the screen
+            if (obj.positionY >= 600) {
+              fallingObjects.remove(obj);
+            }
+          }
+        });
+      }
+    });
+  }
