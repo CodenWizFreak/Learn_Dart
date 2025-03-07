cf1235d4f0add36f3c5edbad32e8baa7e52f0f41
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-03-08 00:07:11 +0530

    Create Basketgame.dart

---
+++ Basketgame.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FallingObjectGame(),
+      ),
+    );
+  }
+}
