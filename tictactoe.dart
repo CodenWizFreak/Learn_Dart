bab3887cefb7e48d0d23c1bf52b00be4c1fac27a
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-03-10 00:19:52 +0530

    Update tictactoe.dart

--- tictactoe.dart
+++ tictactoe.dart
@@ -56,3 +56,29 @@ class _TicTacToeGameState extends State<TicTacToeGame> {
     }
   }
 
+  // Check for a winner
+  void checkWinner() {
+    // Winning combinations
+    List<List<int>> winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var combination in winningCombinations) {
+      if (board[combination[0]] == board[combination[1]] &&
+          board[combination[1]] == board[combination[2]] &&
+          board[combination[0]] != '') {
+        setState(() {
+          gameOver = true;
+          winner = board[combination[0]]; // Set winner (X or O)
+        });
+        return;
+      }
+    }
+
