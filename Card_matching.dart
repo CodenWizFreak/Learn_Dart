017e3140e120ab3a5f6f651057b4d8fda9859815
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-03-25 12:22:16 +0530

    Update Card_matching.dart

--- Card_matching.dart
+++ Card_matching.dart
@@ -16,3 +16,15 @@ class _MemoryCardGameState extends State<MemoryCardGame> with TickerProviderStat
     '🍎', '🍌', '🍓', '🍍', '🍉', '🍒', '🍊', '🍓', '🍇', '🥭', '🍒', '🍑', '🥝', '🍋', '🍔', '🍕',
     '🍪', '🍩', '🍫', '🍿', '🍷', '🍺', '🍻', '🍴', '🥤', '🍲'
   ];
+
+  // List of 12 emojis for the game, randomly selected pairs
+  List<String> cards = [];
+  List<String> revealedCards = List.filled(12, '');
+  int flippedCount = 0;
+  int matchCount = 0;
+  int totalMatches = 6;
+  int score = 0;  // Variable to keep track of the score
+  List<int> flippedIndexes = [];  // Keep track of flipped card indexes
+
+  // To control the flip animation for each card
+  Map<int, AnimationController> flipControllers = {};
