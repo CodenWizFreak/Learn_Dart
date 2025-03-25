92eb0f634b363927408d89b7737acbc07b39312c
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-03-25 12:21:59 +0530

    Update Card_matching.dart

--- Card_matching.dart
+++ Card_matching.dart
@@ -9,3 +9,10 @@ class MemoryCardGame extends StatefulWidget {
   @override
   _MemoryCardGameState createState() => _MemoryCardGameState();
 }
+
+class _MemoryCardGameState extends State<MemoryCardGame> with TickerProviderStateMixin {
+  // Expanded list of available emojis
+  List<String> allEmojis = [
+    '🍎', '🍌', '🍓', '🍍', '🍉', '🍒', '🍊', '🍓', '🍇', '🥭', '🍒', '🍑', '🥝', '🍋', '🍔', '🍕',
+    '🍪', '🍩', '🍫', '🍿', '🍷', '🍺', '🍻', '🍴', '🥤', '🍲'
+  ];
