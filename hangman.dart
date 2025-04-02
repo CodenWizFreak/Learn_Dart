cebeb231640fc9cb5bf6e05214c79d3eaa7e790d
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-04-02 11:41:05 +0530

    Create hangman.dart

---
+++ hangman.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hangman Game',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: HangmanGame(),
+    );
+  }
+}
